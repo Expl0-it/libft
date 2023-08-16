103bf23ebad85a3e436f1d957db6c772ed19b506
unknown <expl0it.root@protonmail.com>
2023-08-16 19:34:08 +0200

    created ft_strdel.c

---
+++ ft_strdel.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+
+void ft_strdel(char **as)
+{
+	if (as != NULL && *as != NULL)
+	{
+		free(*as);
+		*as = NULL;
+	}
+}
+
+/*
+#include <stdio.h>
+int main(void)
+{
+	char	*str = ft_strnew(2);
+	printf("%p\n", str);
+	ft_strdel(&str);
+	printf("%p\n", str);
+	return 0;
+}
+*/
--- libft.h
+++ libft.h
@@ -11,6 +11,7 @@
 void	*ft_memalloc(size_t size);
 void	ft_memdel(void **ap);
 char	*ft_strnew(size_t size);
+void	ft_strdel(char **as);
 
 //bonus
 
