f5524bd2dbb98e6070d69c7803d33d3a68ae90eb
Expl0-it <expl0it.root@protonmail.com>
2023-08-21 08:53:05 +0200

    added ft_strjoin.c

---
+++ ft_strjoin.c
@@ -0,0 +1,33 @@
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int	i;
+	char	*new;
+
+	i = 0;
+	new = ft_strnew(strlen(s1) + strlen(s2));
+	while (*s1)
+	{
+		new[i] = *s1;
+		s1++;
+		i++;
+	}
+	while (*s2)
+	{
+		new[i] = *s2;
+		s2++;
+		i++;
+	}
+	return (new);
+}
+
+/*
+#include <stdio.h>
+int main(void)
+{
+	char	*a = ft_strjoin("abc", "def");
+	printf("%s", a);
+	return (0);
+}
+*/
--- libft.h
+++ libft.h
@@ -18,7 +18,10 @@ void	ft_striter(char *s, void (*f)(char *));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 char	*ft_strmap(char const *s, char (*f)(char));
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-
+//strequ->strcmp
+//strnequ->strncmp
+//strsub
+char	*ft_strjoin(char const *s1, char const *s2);
 //bonus
 
 //additional
