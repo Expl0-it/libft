db2b68c1b2ce233bb4b2db4e853de84e4edb5339
Expl0-it <expl0it.root@protonmail.com>
2023-08-24 10:17:56 +0200

    added ft_memcpy.c

---
+++ ft_memcpy.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	char	*p_dest;
+	char	*p_src;
+
+	p_dest = (char *)dest;
+	p_src = (char *)src;
+	while (n > 0)
+	{
+		p_dest[n - 1] = p_src[n - 1];
+		n--;
+	}
+	return (dest);
+}
+
+/**/
+#include <stdio.h>
+int main(void)
+{
+	char	*a = "abcdefg";
+	char	b[10];
+	char	*p_b = ft_memcpy(b, a, 8);
+	printf("%p, %s", p_b, b);
+	return 0;
+}
