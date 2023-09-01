170718d76591e3103049513702c5a703479350c7
Expl0-it <expl0it.root@protonmail.com>
2023-09-01 23:15:35 +0200

    added ft_memcmp.c

---
+++ ft_memcmp.c
@@ -0,0 +1,30 @@
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*p_s1;
+	unsigned char	*p_s2;
+
+	p_s1 = (unsigned char *)s1;
+	p_s2 = (unsigned char *)s2;
+	while (n-- > 0)
+	{
+		if (*p_s1 != *p_s2)
+			return (*p_s1 - *p_s2);
+		p_s1++;
+		p_s2++;
+	}
+	return (0);
+}
+
+/*
+#include <stdio.h>
+int main(void)
+{
+	char	*a = "abbad";
+	char	*b = "abbax";
+	int num = ft_memcmp(a, b, 5);
+	printf("%d", num);
+	return (0);
+}
+*/
