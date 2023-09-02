ed331fd60fdcf19cc3e27ca89bfb8247a30973dd
Expl0-it <expl0it.root@protonmail.com>
2023-09-02 22:33:11 +0200

    added ft_strlcpy.c

---
+++ ft_strlcpy.c
@@ -0,0 +1,35 @@
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	length;
+
+	length = 0;
+	if (size == 0)
+	{
+		while (src[length])
+			length++;
+		return (length);
+	}
+	while (length < size - 1 && src[length] != '\0')
+	{
+		dst[length] = src[length];
+		length++;
+	}
+	if (length < size)
+		dst[length] = '\0';
+	while (src[length] != '\0')
+		length++;
+	return (length);
+}
+
+/*
+#include <stdio.h>
+int main(void)
+{
+	char	str[10];
+	size_t a = ft_strlcpy(str, "abcdfqewfef", 5);
+	printf("%d, %s", a, str);
+	return (0);
+}
+*/
--- libft.h
+++ libft.h
@@ -14,6 +14,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
 int	ft_strlen(char const *str);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 char	*strchr(const char *s, int c);
 //second
 void	*ft_memalloc(size_t size);
