2c3e41882109df2c11cabcf575c065f2679cbec7
Expl0-it <expl0it.root@protonmail.com>
2023-09-04 18:21:46 +0200

    modified libft.h to include ft_strchr properly

--- libft.h
+++ libft.h
@@ -16,7 +16,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n);
 int	ft_strlen(char const *str);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
-char	*strchr(const char *s, int c);
+char	*ft_strchr(const char *s, int c);
 //second
 void	*ft_memalloc(size_t size);
 void	ft_memdel(void **ap);
