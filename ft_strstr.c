6d82561aef4920b0ed55621e7556c0290f8cd2cd
Expl0-it <expl0it.root@protonmail.com>
2023-09-05 21:43:57 +0200

    added ft_strstr.c

---
+++ ft_strstr.c
@@ -0,0 +1,35 @@
+#include "libft.h"
+
+char	*ft_strstr(const char *haystack, const char *needle)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (needle == NULL || needle[0] == '\0')
+		return ((char *)haystack);
+	while (haystack[i])
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j] && needle[j] != 0)
+			j++;
+		if (needle[j] == 0)
+		{
+			haystack += i;
+			return ((char *)haystack);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+/**/
+#include <string.h>
+#include <stdio.h>
+int main(void)
+{
+	char *a = ft_strstr("abcdbadx", "bad");
+	char *b = strstr("abacbadx", "bad");
+	printf("%s, %s", a, b);
+	return 0;
+}
--- libft.h
+++ libft.h
@@ -18,6 +18,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_strstr(const char *haystack, const char *needle);
 //second
 void	*ft_memalloc(size_t size);
 void	ft_memdel(void **ap);
