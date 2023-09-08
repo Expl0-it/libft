13b266bcc4eda4c7994de701f8e4eb23a9e24e71
Expl0-it <expl0it.root@protonmail.com>
2023-09-08 22:00:50 +0200

    added ft_isalpha.c ensuring type safety on check

---
+++ ft_isalpha.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+int	ft_isalpha(int c)
+{
+	if (('A' <= (unsigned char)c && (unsigned char)c <= 'Z')
+	|| ('a' <= (unsigned char)c && (unsigned char)c <= 'z'))
+		return (1);
+	return (0);
+}
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+	int	a = ft_isalpha('A');
+	printf("%d", a);
+	return (0);
+}
+*/
