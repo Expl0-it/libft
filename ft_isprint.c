a6be7b6693ab86d03217418594292b7750b914c0
Expl0-it <90573515+Expl0-it@users.noreply.github.com>
2023-09-10 17:11:00 +0200

    added ft_isprint.c

---
+++ ft_isprint.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+
+int	ft_isprint(int c)
+{
+	if (' ' <= (unsigned char)c && (unsigned char)c <= '~')
+		return (1);
+	return (0);
+}
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+	int	a = ft_isprint('\0');
+	printf("%d", a);
+	return (0);
+}
+*/
