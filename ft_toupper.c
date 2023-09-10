31e8f66a3e74754ab7f279e559a06499796531e8
Expl0-it <90573515+Expl0-it@users.noreply.github.com>
2023-09-10 17:21:52 +0200

    added ft_toupper.c

---
+++ ft_toupper.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+int	ft_toupper(int c)
+{
+	if ('a' <= (unsigned char)c && (unsigned char)c <= 'z')
+		c -= 32;
+	return (c);
+}
+
+/**/
+#include <stdio.h>
+int	main(void)
+{
+	int	a = ft_toupper('a');
+	printf("%c", a);
+	return (0);
+}
