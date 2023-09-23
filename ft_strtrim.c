4986a8ee2303c2ba67e0688f153980971d875cbe
Expl0-it <90573515+Expl0-it@users.noreply.github.com>
2023-09-23 21:09:11 +0200

    added ft_strtrim.c

---
+++ ft_strtrim.c
@@ -0,0 +1,31 @@
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+	char	*trimmed;
+
+	trimmed = NULL;
+	if (s1 && set)
+	{
+		start = 0;
+		end = ft_strlen(s1);
+		while (s1[start] && ft_strchr(set, s1[start]))
+			start++;
+		while (s1[end - 1] && ft_strchr(set, s1[end - 1]))
+			end--;
+		trimmed = ft_strnew(end - start);
+		ft_strlcpy(trimmed, &s1[start], end - start + 1);
+	}
+	return (trimmed);
+}
+
+/**/
+#include <stdio.h>
+int	main(void)
+{
+	char	*a = ft_strtrim("   \t  abc      ", "\t c");
+	printf("%s", a);
+	return (0);
+}
