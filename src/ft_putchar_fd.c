f1e6101e69abaffdb66bd709669446d749e064c8
Expl0-it <90573515+Expl0-it@users.noreply.github.com>
2023-11-02 18:17:35 +0100

    added documentation to ft_putchar_fd.c

--- src/ft_putchar_fd.c
+++ src/ft_putchar_fd.c
@@ -1,3 +1,12 @@
 #include "../includes/libft.h"
 
+/** @brief Put a character in file
+ * 
+ * 	Outputs the character ’c’ to the given file
+ * 	descriptor.
+ * 
+ * 	@param c The character to output
+ * 	@param c The file descriptor on which to write
+ *  @return Void
+ */
 void	ft_putchar_fd(char c, int fd);
