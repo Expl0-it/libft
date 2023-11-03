ec16e61fc60447ecb847c393f556afef797cb736
Expl0-it <90573515+Expl0-it@users.noreply.github.com>
2023-11-03 12:36:27 +0100

    compleated ft_putchar_fd.c

--- src/ft_putchar_fd.c
+++ src/ft_putchar_fd.c
@@ -3,10 +3,24 @@
 /** @brief Put a character in file.
  * 
  * 	Outputs the character ’c’ to the given file
- * 	descriptor.
+ * 	descriptor. \n	 	
+ * 	STDFD:
+ * 	0 => stdin;
+ * 	1 => stdout;
+ * 	2 => stderr;
  * 
  * 	@param c The character to output
- * 	@param c The file descriptor on which to write
+ * 	@param fd The file descriptor on which to write
  *  @return Void.
  */
-void	ft_putchar_fd(char c, int fd);
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+/**/
+int main(void)
+{
+	ft_putchar_fd('c', 1);
+	return (0);
+}
