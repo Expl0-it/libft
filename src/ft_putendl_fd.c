80828ecc99895c704ce6e942d1bce30744719e85
Expl0-it <90573515+Expl0-it@users.noreply.github.com>
2023-11-06 21:20:06 +0100

    compleated ft_putendl_fd.c

---
+++ src/ft_putendl_fd.c
@@ -0,0 +1,27 @@
+#include "../includes/libft.h"
+
+/** @brief Put a character in file.
+ * 
+ * 	Outputs the string ’s’ to the given file
+ * 	descriptor, followed by a newline.
+ * 	STDFD:
+ * 	0 => stdin;
+ * 	1 => stdout;
+ * 	2 => stderr;
+ * 
+ * 	@param s The string to output
+ * 	@param fd The file descriptor on which to write
+ *  @return Void.
+ */
+void	ft_putendl_fd(char *s, int fd)
+{
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
+}
+
+/**/
+int main(void)
+{
+	ft_putendl_fd("abc", 1);
+	return (0);
+}
