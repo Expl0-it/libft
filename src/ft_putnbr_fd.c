1a00b49862fe84c2c2956cd226d4e73afcc9acab
Expl0-it <90573515+Expl0-it@users.noreply.github.com>
2023-11-12 17:56:17 +0100

    compleated ft_putnbr_fd.c

--- src/ft_putnbr_fd.c
+++ src/ft_putnbr_fd.c
@@ -15,5 +15,29 @@
  */
 void	ft_putnbr_fd(int n, int fd)
 {
-	
+		if (n == INT_MIN)
+	{
+		ft_putstr_fd("-2147483648", fd);
+		return ;
+	}
+	else if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n = -n;
+	}
+	if (n >= 10)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		n = n % 10;
+	}
+	if (n < 10)
+		ft_putchar_fd(n + 48, fd);
 }
+
+/*
+int	main(void)
+{
+	ft_putnbr_fd(12345, 1);
+	return (0);
+}
+*/
